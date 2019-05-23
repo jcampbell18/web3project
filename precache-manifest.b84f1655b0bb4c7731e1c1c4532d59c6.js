e2c77593de4ea34de47979f93724b64b402bdaaf
Jason C (Desktop) <43692594+jcampbell18@users.noreply.github.com>
2019-05-22 17:40:18 -0700

    Updates

---
+++ precache-manifest.b84f1655b0bb4c7731e1c1c4532d59c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20bc529686986ab58fbee255ed9c18d",
+    "url": "/web3project/index.html"
+  },
+  {
+    "revision": "dc3b43c18aca91e27963",
+    "url": "/web3project/static/css/main.a234b58e.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/web3project/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "dc3b43c18aca91e27963",
+    "url": "/web3project/static/js/main.91134968.chunk.js"
+  },
+  {
+    "revision": "8687f2165b5f00a55ae8",
+    "url": "/web3project/static/js/runtime~main.6284cdd1.js"
+  },
+  {
+    "revision": "2858248d55735d9be369ca9aea1e24c5",
+    "url": "/web3project/static/media/reel.2858248d.jpg"
+  }
+]);
